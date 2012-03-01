20f57e9878685163c4435fda6c12cb2ad400356f
can <can@canmatoMacBook-Pro.local>
2012-03-01 21:06:32 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shu::Application.config.secret_token = 'ddc238d9d6727cef494ce5058a9d5a21de7c3f7ff8f7b16469d2db32266e527e3fe5602b9b709b6c0c258ad01cd7d66bd7ab0ac7070d2c70dd5eb241521d4c7c'
