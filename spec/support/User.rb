02869f47dbb08543d4a7bc90e7919c94ae0d0484
cantin <cantin2010@gmail.com>
2012-04-15 11:30:42 +0800

    Fixed user_login

--- spec/support/User.rb
+++ spec/support/User.rb
@@ -1,5 +1,6 @@
+#encoding: utf-8
 def user_create
-  u = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: '123456', password_confirmation: '123456')
+  u = User.new(name: Faker::Name.name, email: "test@shu.com", password: '123456', password_confirmation: '123456')
   u.confirm!
   u
 end
@@ -8,5 +9,5 @@ def user_login(user)
   visit '/users/sign_in'
   fill_in "user_email", :with=>user.email
   fill_in "user_password", :with=>user.password
-  click_button "Sign in"
+  click_button "登录"
 end
